3cfaef792a8dd111265455e177b4d569cd10ece1
GM <m.glazowski06@gmail.com>
2020-05-17 15:11:34 +0200

    Praca domowa - wyjątki i ich obsługa; weryfikacja niezerowej długości boków

--- src/main/java/zadankaDomowePartThree/wyjatki/trojkat/AppTrojkat.java
+++ src/main/java/zadankaDomowePartThree/wyjatki/trojkat/AppTrojkat.java
@@ -4,7 +4,7 @@ public class AppTrojkat {
     public static void main(String[] args) {
         System.out.println("teszt");
 
-        Trojkat trojkat = new Trojkat(3, 4, 1);
+        Trojkat trojkat = new Trojkat(4,3,5 );
         try {
             SprawdzanieKreacjiTrojkata.czyTrojkatMoznaSkonstruowac(trojkat);
         } catch (ToNieMozeBycTrojkatException e) {
--- src/main/java/zadankaDomowePartThree/wyjatki/trojkat/SprawdzanieKreacjiTrojkata.java
+++ src/main/java/zadankaDomowePartThree/wyjatki/trojkat/SprawdzanieKreacjiTrojkata.java
@@ -2,18 +2,13 @@ package zadankaDomowePartThree.wyjatki.trojkat;
 
 public class SprawdzanieKreacjiTrojkata {
     public static void czyTrojkatMoznaSkonstruowac(Trojkat trojkat) throws ToNieMozeBycTrojkatException {
-        if (trojkat.getC() == Math.sqrt(Math.pow(trojkat.getA(), 2) + Math.pow(trojkat.getB(), 2))) {
+        if (trojkat.getA() == 0 && trojkat.getB() == 0 && trojkat.getC() == 0) {
+            throw new ToNieMozeBycTrojkatException("Nie można skontruować tego trójkąta");
+        } else if (trojkat.getC() == Math.sqrt(Math.pow(trojkat.getA(), 2) + Math.pow(trojkat.getB(), 2)) ||
+                trojkat.getB() == Math.sqrt(Math.pow(trojkat.getA(), 2) + Math.pow(trojkat.getC(), 2)) ||
+                trojkat.getA() == Math.sqrt(Math.pow(trojkat.getC(), 2) + Math.pow(trojkat.getB(), 2))) {
             System.out.println("Ten trójkąt składa się z trzech boków: " +
                     trojkat.getA() + ", " + trojkat.getB() + ", " + trojkat.getC());
-
-        } else if (trojkat.getB() == Math.sqrt(Math.pow(trojkat.getA(), 2) + Math.pow(trojkat.getC(), 2))) {
-            System.out.println("Ten trójkąt składa się z trzech boków: " +
-                    trojkat.getA() + ", " + trojkat.getB() + ", " + trojkat.getC());
-
-        } else if (trojkat.getA() == Math.sqrt(Math.pow(trojkat.getC(), 2) + Math.pow(trojkat.getB(), 2))) {
-            System.out.println("Ten trójkąt składa się z trzech boków: " +
-                    trojkat.getA() + ", " + trojkat.getB() + ", " + trojkat.getC());
-
         } else {
             throw new ToNieMozeBycTrojkatException("Nie można skontruować tego trójkąta");
         }
